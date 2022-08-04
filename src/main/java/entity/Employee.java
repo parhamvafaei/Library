307ea07c5943c0ca127af7b4b47d16cfe6b8852e
Amin Azarakhshi <azarakhshi.mohammadamin@gmail.com>
2022-08-04 16:52:26 +0430

    create User & Employee classes

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -1,6 +1,13 @@
 package entity;
 
-public class Employee extends Person{
+public class Employee extends Person {
+
     double salary;
+
+    public Employee(int id, String name, Role role, double salary) {
+        super(id, name, role);
+        this.salary = salary;
+    }
+
 
 }
--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -4,4 +4,13 @@ abstract class Person {
     int id;
     String name;
     Role role ;
+
+    public Person(int id, String name, Role role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+    public Person(){
+
+    }
 }
