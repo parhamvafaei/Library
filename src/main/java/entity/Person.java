235515cf02040bc79971758277aa2f3b88edc809
bidkhonei <hassan317zahedi@gmail.com>
2022-08-04 15:06:54 +0330

    Create a set

--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -4,4 +4,16 @@ abstract class Person {
     int id;
     String name;
     Role role ;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
